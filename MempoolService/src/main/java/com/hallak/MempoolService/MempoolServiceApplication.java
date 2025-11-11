a995ce777fb70c92f6e935ea16e40622224a2e4c
Mzg. <jchallaksantos@gmail.com>
2025-11-11 15:13:48 -0300

    ++MempoolService

---
+++ MempoolService/src/main/java/com/hallak/MempoolService/MempoolServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hallak.MempoolService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MempoolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MempoolServiceApplication.class, args);
+	}
+
+}
---
+++ MempoolService/src/test/java/com/hallak/MempoolService/MempoolServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hallak.MempoolService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MempoolServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
